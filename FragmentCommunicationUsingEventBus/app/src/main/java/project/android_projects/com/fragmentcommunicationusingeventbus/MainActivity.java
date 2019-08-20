7f19cf93fd37275baa1bb445ab27251c2deee374
Shu Zhang <shu@goopter.com>
2019-08-20 14:45:15 -0700

    fixed duplicated fragment ui problem

--- FragmentCommunicationUsingEventBus/app/src/main/java/project/android_projects/com/fragmentcommunicationusingeventbus/MainActivity.java
+++ FragmentCommunicationUsingEventBus/app/src/main/java/project/android_projects/com/fragmentcommunicationusingeventbus/MainActivity.java
@@ -1,6 +1,7 @@
 package project.android_projects.com.fragmentcommunicationusingeventbus;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -22,21 +23,24 @@ public class MainActivity extends AppCompatActivity {
         fragmentMgr = getSupportFragmentManager();
         fragmentTrans = fragmentMgr.beginTransaction();
 
-        if(savedInstanceState == null){
-            initSendFragment();
-            initReceiveFragment();
-            fragmentTrans.commit();
-        }
+        initSendFragment();
+        initReceiveFragment();
+        fragmentTrans.commit();
     }
 
     private void initSendFragment() {
         fragmentSend = new FragmentSendText();
-        fragmentTrans.add(R.id.send_fragment, fragmentSend);
-
+        if(fragmentSend != null){
+            fragmentTrans.add(R.id.send_fragment, fragmentSend);
+        }
+        Log.d("NULL Fragment", "FragmentSend is null");
     }
 
     private void initReceiveFragment() {
         fragmentReceive = new FragmentReceiveText();
-        fragmentTrans.add(R.id.receive_fragment, fragmentReceive);
+        if(fragmentReceive != null){
+            fragmentTrans.add(R.id.receive_fragment, fragmentReceive);
+        }
+        Log.d("NULL Fragment", "FragmentReceive is null");
     }
 }
