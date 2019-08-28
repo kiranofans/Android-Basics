319051daa174121febf1822e09d4fd4f7e472adc
Shu Zhang <shu@goopter.com>
2019-08-27 23:05:23 -0700

    Begin to use RxJava

---
+++ RxJava_RxAndroid_Example/app/src/main/java/project/android_projects/com/rxjava_rxandroid_example/MainActivity.java
@@ -0,0 +1,20 @@
+package project.android_projects.com.rxjava_rxandroid_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/** RxJava (React Extension) is a combination of
+ * the best ideas from the Observer pattern, Iterator pattern,
+ * and functional programming. */
+public class MainActivity extends AppCompatActivity {
+
+    /** RxJava is a library for composing asynchronous and event-based
+     * programming by using obsrvable sequence */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
