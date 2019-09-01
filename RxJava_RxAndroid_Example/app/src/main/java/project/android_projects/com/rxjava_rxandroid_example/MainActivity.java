a99b65bfffaf85b99379a544e7b0206911777780
Shu Zhang <shu@goopter.com>
2019-08-31 21:40:43 -0700

    added completable and single

--- RxJava_RxAndroid_Example/app/src/main/java/project/android_projects/com/rxjava_rxandroid_example/MainActivity.java
+++ RxJava_RxAndroid_Example/app/src/main/java/project/android_projects/com/rxjava_rxandroid_example/MainActivity.java
@@ -1,29 +1,35 @@
 package project.android_projects.com.rxjava_rxandroid_example;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
 
 import io.reactivex.BackpressureStrategy;
+import io.reactivex.Completable;
+import io.reactivex.CompletableEmitter;
+import io.reactivex.CompletableOnSubscribe;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableEmitter;
 import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.Maybe;
 import io.reactivex.Observable;
-import io.reactivex.Scheduler;
+import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.observers.DisposableMaybeObserver;
 import io.reactivex.observers.DisposableObserver;
+import io.reactivex.observers.DisposableSingleObserver;
 import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subscribers.DisposableSubscriber;
 
 /**
  * RxJava (React Extension) is a combination of
@@ -39,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     private View observable, flowable, single, maybe, completable;
 
     private List<String> resultList;
+
     /**
      * RxJava is a library for composing asynchronous and event-based
      * programming by using observable sequence
@@ -57,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
         createObservableType();
         maybeType();
         flowableType();
+        singleType();
+        completableType();
     }
 
     @Override
@@ -97,6 +106,16 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private Predicate<Integer> byEvenNums() {
+        return new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer integer) {
+                return integer % 2 == 0;
+            }
+        };
+    }
+
     private void createObservableType() {
         /** Emits 0 or n items and terminates with an success or an error event. */
         Observable<String> observableList = Observable
@@ -110,6 +129,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onNext(String s) {
                 resultList.add(s);
+                obsv_detailTV.append("\n------------------\nSubscriveOn\n");
                 for (String strs : resultList) {//user foreach to iterate
                     obsv_detailTV.append(strs + ", ");
                 }
@@ -133,6 +153,26 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();*/
             }
         });
+        observableList.subscribeWith(new DisposableObserver<String>() {
+            @Override
+            public void onNext(String s) {
+                resultList.add(s);
+                obsv_detailTV.append("\nSubscribeWith\n");
+                for (String strs : resultList) {//user foreach to iterate
+                    obsv_detailTV.append(strs + ", ");
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
     }
 
     private void maybeType() {
@@ -141,10 +181,11 @@ public class MainActivity extends AppCompatActivity {
         resultList = new ArrayList<>();
         Disposable disposable = Maybe.just("Maroon").delay(10, TimeUnit.SECONDS,
                 Schedulers.io()).subscribeWith(new DisposableMaybeObserver<String>() {
+
             @Override
             protected void onStart() {
                 super.onStart();
-                Toast.makeText(MainActivity.this,"Maybe Started",
+                Toast.makeText(MainActivity.this, "Maybe Started",
                         Toast.LENGTH_SHORT).show();
                 // maybe_detailTV.append("OnStart");
             }
@@ -152,21 +193,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(String s) {
                 resultList.add(s);
-                for(String str:resultList){
-                    maybe_detailTV.append("Maybe Success: "+ str);
+                for (String str : resultList) {
+                    maybe_detailTV.append("Maybe Success: " + str);
                 }
-                maybe_detailTV.append("Maybe Success: "+ s);
+                maybe_detailTV.append("Maybe Success: " + s);
             }
 
             @Override
             public void onError(Throwable e) {
-                Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onComplete() {
                 Toast.makeText(MainActivity.this,
-                        "All Maybe data emitted!",Toast.LENGTH_SHORT).show();
+                        "All Maybe data emitted!", Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -174,7 +215,7 @@ public class MainActivity extends AppCompatActivity {
         disposable.dispose();
     }
 
-    private void flowableType(){
+    private void flowableType() {
         /** Emits 0 or n items and terminates with an success or an error event.
          * Supports backpressure, which allows to control how fast a source emits items. */
         Flowable<String> flowable = Flowable.create(new FlowableOnSubscribe<String>() {
@@ -186,8 +227,8 @@ public class MainActivity extends AppCompatActivity {
                 //could be some blocking operation
                 Thread.sleep(1000);
 
-                //The consumer (observer) might have canceled the flow
-                if(emitter.isCancelled()){
+                //The consumer might have canceled the flow
+                if (emitter.isCancelled()) {
                     return;
                 }
 
@@ -204,8 +245,50 @@ public class MainActivity extends AppCompatActivity {
         }, BackpressureStrategy.BUFFER);
 
         flowable_detailTV.append("Subscribe!\n");
-        flowable_detailTV.append("Flowable type has been "+flowable.subscribe().toString());
+        flowable_detailTV.append("Flowable type has been " + flowable.subscribe().toString());
         flowable_detailTV.append("\nDone!");
+    }
+
+    private void singleType() {
+        // It always emits one value or an error notification
+        //So there are only two methods: onSuccess() and onError()
+        Disposable disposable = Single.just("Single Item").delay(10, TimeUnit.SECONDS,
+                Schedulers.io()).subscribeWith(new DisposableSingleObserver<String>() {
+
+            /*@Override
+            protected void onStart() {
+                super.onStart();
+                single_detailTV.append("Started");
+            }*/
+
+            @Override
+            public void onSuccess(String s) {
+                single_detailTV.append(s);
+                Toast.makeText(MainActivity.this, "Success: " + s,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+        });
+        disposable.dispose();
+    }
+
+    private void completableType() {
+        Completable completable = Completable.create(new CompletableOnSubscribe() {
+            @Override
+            public void subscribe(CompletableEmitter emitter) throws Exception {
+                emitter.onComplete();
+                if (emitter.isDisposed()) {
+                    return;
+                }
+            }
+        });
+
+        completable_detailTV.append("Completable State: " + completable.subscribe().toString());
+
     }
 }
 
