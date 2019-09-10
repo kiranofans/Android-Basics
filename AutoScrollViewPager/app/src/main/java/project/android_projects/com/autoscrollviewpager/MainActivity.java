baed897a54a5bf15d70663db5b145d3f42383ad2
Shu Zhang <shu@goopter.com>
2019-09-09 17:54:54 -0700

    Hide support action bar

--- AutoScrollViewPager/app/src/main/java/project/android_projects/com/autoscrollviewpager/MainActivity.java
+++ AutoScrollViewPager/app/src/main/java/project/android_projects/com/autoscrollviewpager/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();//Hide support action bar
 
         AutoScrollPagerAdapter pagerAdp = new AutoScrollPagerAdapter(getSupportFragmentManager());
 
